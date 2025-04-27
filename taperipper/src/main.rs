731ddacc9e4680c9fab07dea2f44d1258027c9c7
Aki Van Ness <aki@lethalbit.net>
2025-04-27 14:02:35 -0700

    Added initial UEFI environment setup

--- taperipper/src/main.rs
+++ taperipper/src/main.rs
@@ -2,4 +2,10 @@
 
 #![feature(uefi_std, panic_payload_as_str, panic_can_unwind)]
 
-fn main() {}
+mod uefi_sys;
+fn main() {
+    // Setup the UEFI crate
+    crate::uefi_sys::init_uefi();
+    crate::uefi_sys::shutdown_now();
+}
+
---
+++ taperipper/src/uefi_sys.rs
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// UEFI Helpers/Utilities
+
+use std::os::uefi as uefi_std;
+use tracing::info;
+use uefi::{
+    Handle, Status, boot,
+    runtime::{self, ResetType},
+    table,
+};
+
+pub fn init_uefi() {
+    // Get the system table and image handle
+    let system_table = uefi_std::env::system_table();
+    let image_handle = uefi_std::env::image_handle();
+
+    // Setup the UEFI crate
+    unsafe {
+        table::set_system_table(system_table.as_ptr().cast());
+        let image_handle = Handle::from_ptr(image_handle.as_ptr().cast()).unwrap();
+        boot::set_image_handle(image_handle);
+    }
+}
+
+pub fn has_boot_services() -> bool {
+    // Try to get the System table
+    let Some(system_table) = table::system_table_raw() else {
+        return false;
+    };
+
+    // Get a handle to the system table and check if the boot services are there
+    let system_table = unsafe { system_table.as_ref() };
+    return !system_table.boot_services.is_null();
+}
+
+// Reboot the machine
+pub fn reboot(status: Option<Status>, data: Option<&[u8]>) -> ! {
+    info!("Rebooting system");
+
+    runtime::reset(ResetType::COLD, status.unwrap_or(Status::SUCCESS), data);
+}
+pub fn reboot_now() -> ! {
+    reboot(None, None);
+}
+
+// Shutdown the machine
+pub fn shutdown(status: Option<Status>, data: Option<&[u8]>) -> ! {
+    info!("Shutting system down");
+
+    runtime::reset(ResetType::SHUTDOWN, status.unwrap_or(Status::SUCCESS), data);
+}
+pub fn shutdown_now() -> ! {
+    shutdown(None, None);
+}
