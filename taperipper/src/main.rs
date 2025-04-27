dba390f605282f11a903b1149f916c6533e1b7b6
Aki Van Ness <aki@lethalbit.net>
2025-04-27 14:04:43 -0700

    taperipper: Added simple panic handler

--- taperipper/src/main.rs
+++ taperipper/src/main.rs
@@ -2,10 +2,23 @@
 
 #![feature(uefi_std, panic_payload_as_str, panic_can_unwind)]
 
+use core::arch::asm;
+use std::panic;
 mod uefi_sys;
 fn main() {
     // Setup the UEFI crate
     crate::uefi_sys::init_uefi();
+    // Hook the defaualt std panic handler
+    panic::set_hook(Box::new(|pi| panic(pi)));
     crate::uefi_sys::shutdown_now();
 }
 
+pub fn panic(info: &panic::PanicHookInfo<'_>) -> ! {
+    // TODO(aki): Maybe one day we'll get stack unwinding
+
+    loop {
+        unsafe {
+            asm!("hlt", options(nomem, nostack));
+        }
+    }
+}
