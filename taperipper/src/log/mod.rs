af25e3e7eb9982a717a611abdc9984b75f661043
Aki Van Ness <aki@lethalbit.net>
2025-04-27 16:37:23 -0700

    taperipper: log: Added QEMU debugcon trace logger layer

--- taperipper/src/log/mod.rs
+++ taperipper/src/log/mod.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+pub mod qemu;
 pub mod tracer;
 pub mod writer;
 
+pub use qemu::QEMUDebugcon;
 pub use tracer::ConsoleSubscriber;
---
+++ taperipper/src/log/qemu.rs
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// This is a logging interface for the tracing subscriber that emits
+// the log messages into the QEMU Debugcon device.
+//
+// Using this as the secondary fallback logger lets us see log messages if somehow
+// the primary logging output fails.
+//
+// The default IO port is `0xE9` as noted in the qemu documentation, but it can be
+// remapped.
+
+use core::{arch::asm, fmt};
+use tracing::Metadata;
+
+use crate::{
+    display::{color, style},
+    log::writer,
+};
+
+#[derive(Clone, Copy, Debug)]
+pub struct QEMUDebugcon {}
+
+impl QEMUDebugcon {
+    const PORT: u16 = 0xE9;
+}
+
+impl Default for QEMUDebugcon {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl<'a> writer::LogOutput<'a> for QEMUDebugcon {
+    type Writer = Self;
+
+    #[inline]
+    fn make_writer(&'a self) -> Self::Writer {
+        QEMUDebugcon {}
+    }
+
+    #[cfg(debug_assertions)]
+    #[inline]
+    fn enabled(&self, _metadata: &Metadata<'_>) -> bool {
+        true
+    }
+
+    #[cfg(not(debug_assertions))]
+    #[inline]
+    fn enabled(&self, _metadata: &Metadata<'_>) -> bool {
+        false
+    }
+
+    #[inline]
+    fn line_len(&self) -> usize {
+        160
+    }
+}
+
+#[cfg(debug_assertions)]
+impl fmt::Write for QEMUDebugcon {
+    #[inline]
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        for &byte in s.as_bytes() {
+            unsafe {
+                asm!("outb %al, %dx", in("al") byte, in("dx") Self::PORT, options(att_syntax));
+            }
+        }
+        Ok(())
+    }
+
+    #[inline]
+    fn write_char(&mut self, c: char) -> std::fmt::Result {
+        let mut bytes = [0; 4];
+        c.encode_utf8(&mut bytes);
+
+        for &byte in bytes[0..c.len_utf8()].iter() {
+            unsafe {
+                asm!("outb %al, %dx", in("al") byte, in("dx") Self::PORT, options(att_syntax));
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(not(debug_assertions))]
+impl fmt::Write for QEMUDebugcon {
+    #[inline]
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        // NOP
+        Ok(())
+    }
+}
+
+impl color::SetFgColor for QEMUDebugcon {
+    #[inline]
+    fn set_fg_color(&mut self, _color: color::Color) {
+        // NOP
+    }
+
+    #[inline]
+    fn get_fg_color(&self) -> color::Color {
+        color::Color::Default
+    }
+}
+
+impl color::SetBgColor for QEMUDebugcon {
+    #[inline]
+    fn set_bg_color(&mut self, _color: color::Color) {
+        // NOP
+    }
+
+    #[inline]
+    fn get_bg_color(&self) -> color::Color {
+        color::Color::Default
+    }
+}
+
+impl color::SetColors for QEMUDebugcon {}
+
+impl style::SetStyle for QEMUDebugcon {
+    #[inline]
+    fn set_style(&mut self, _style: style::Style) {
+        // NOP
+    }
+
+    #[inline]
+    fn get_style(&self) -> style::Style {
+        style::Style::None
+    }
+}
