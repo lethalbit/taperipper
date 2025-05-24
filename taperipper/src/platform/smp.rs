42519e5e44c3e8e38dbfb4d81b1e2ee3f3af8eb5
Aki Van Ness <aki@lethalbit.net>
2025-05-24 14:24:13 -0700

    taperipper: Updated the comment in the SMP module and updated the `MAX_CORES` value

--- taperipper/src/platform/smp.rs
+++ taperipper/src/platform/smp.rs
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-// How many max possible cores we want to support
-pub static MAX_CORES: usize = 64;
+// How many possible CPU cores we want to support,
+// Value should be between 2 and 65536 where log₂(n) ∈ ℤ⁺
+// Picked at random by rolling a d20 until it was (0..=15)
+pub static MAX_CORES: usize = 2048;
