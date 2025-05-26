7ad599100ea0ab757c527eee3da7cc41e654b61b
Aki Van Ness <aki@lethalbit.net>
2025-05-26 06:37:59 -0700

    taperipper: Added sme initial ACPI table handling

--- taperipper/src/platform/acpi.rs
+++ taperipper/src/platform/acpi.rs
@@ -1,1 +1,49 @@
 // SPDX-License-Identifier: BSD-3-Clause
+
+use std::{cell::OnceCell, ptr::NonNull};
+
+use acpi::AcpiTables;
+use maitake_sync::{Mutex, spin::InitOnce};
+use tracing::{debug, trace, warn};
+
+use crate::platform;
+
+#[derive(Clone, Debug)]
+pub struct Handler {}
+
+// TODO(aki): We need to write our own allocator eventually:tm: but for now just use identity mapping
+impl acpi::AcpiHandler for Handler {
+    unsafe fn map_physical_region<T>(
+        &self,
+        physical_address: usize,
+        size: usize,
+    ) -> acpi::PhysicalMapping<Self, T> {
+        unsafe {
+            acpi::PhysicalMapping::new(
+                physical_address,
+                NonNull::new(physical_address as *mut T).unwrap(),
+                size,
+                size,
+                self.clone(),
+            )
+        }
+    }
+
+    fn unmap_physical_region<T>(region: &acpi::PhysicalMapping<Self, T>) {}
+}
+
+pub static ACPI_TABLES: InitOnce<Mutex<AcpiTables<Handler>>> = InitOnce::uninitialized();
+
+pub fn init_tables() {
+    let handler = Handler {};
+
+    if let Some((version, address)) = platform::uefi::get_acpi_table() {
+        debug!("ACPI v{} Address: {:#018x}", version, address as usize);
+
+        let tbl = unsafe { acpi::AcpiTables::from_rsdp(handler, address as usize).unwrap() };
+
+        ACPI_TABLES.init(Mutex::new(tbl));
+    } else {
+        warn!("Was unable to initialize ACPI Tables!");
+    }
+}
